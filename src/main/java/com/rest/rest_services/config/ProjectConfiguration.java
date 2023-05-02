dc73a2af67d65f39edc7c3eb5902f70dc383cbaa
Yassine Yazidi <104203113+yazidiyassine@users.noreply.github.com>
2023-05-02 15:44:17 +0100

    using RestTemplate (Old-fashionned)

--- src/main/java/com/rest/rest_services/config/ProjectConfiguration.java
+++ src/main/java/com/rest/rest_services/config/ProjectConfiguration.java
@@ -1,9 +1,11 @@
 package com.rest.rest_services.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
 
@@ -19,5 +21,12 @@ public class ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthenticationInterceptor(){
         return new BasicAuthRequestInterceptor("admin@gmail.com", "admin");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication("admin@gmail.com", "admin").build();
+
     }
 }
--- src/main/java/com/rest/rest_services/controller/ContactController.java
+++ src/main/java/com/rest/rest_services/controller/ContactController.java
@@ -1,10 +1,14 @@
 package com.rest.rest_services.controller;
 
 import com.rest.rest_services.model.Contact;
+import com.rest.rest_services.model.Response;
 import com.rest.rest_services.proxy.ContactProxy;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,13 +16,27 @@ import java.util.List;
 public class ContactController {
 
     private final ContactProxy contactProxy;
+    private final RestTemplate restTemplate;
 
-    public ContactController(ContactProxy contactProxy) {
+    public ContactController(ContactProxy contactProxy, RestTemplate restTemplate) {
         this.contactProxy = contactProxy;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status") String status){
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact){
+        String uri = "http://localhost:8080/api/contact/saveMsg";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("invocationFrom", "RestTemplate");
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact, headers);
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
+                httpEntity, Response.class);
+        return responseEntity;
+
     }
 }
